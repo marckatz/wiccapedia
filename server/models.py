9c0896af67b9837b5a494bab9b859104cb06ed1f
Yu Wu <yw3748@columbia.edu>
2023-08-09 09:05:28 -0400

    add password

--- server/models.py
+++ server/models.py
@@ -2,7 +2,8 @@ from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 import difflib
 
-from config import db
+from config import db, bcrypt
+
 
 # Models go here!
 class User(db.Model, SerializerMixin):
@@ -11,10 +12,28 @@ class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String)
 
+    # add name and password
+    _password_hash = db.Column(db.String, nullable=False)
+
     edits = db.relationship('Edit', back_populates='user', cascade='all,delete-orphan')
     pages = association_proxy('edits', 'page')
 
-    serialize_rules = ('-edits.user',)
+    serialize_rules = ('-edits.user', '-_password_hash',)
+
+    # add password property
+    @property
+    def password_hash(self):
+        return self._password_hash
+
+    @password_hash.setter
+    def password_hash(self, new_password_string):
+        plain_byte_obj = new_password_string.encode('utf-8')
+        encrypted_hash_object = bcrypt.generate_password_hash(plain_byte_obj)
+        hash_object_as_string = encrypted_hash_object.decode('utf-8')
+        self._password_hash = hash_object_as_string
+
+    def authenticate(self, some_string):
+        return bcrypt.check_password_hash(self.password_hash, some_string.encode('utf-8'))
 
     def __repr__(self):
         return f'<User {self.id} {self.username}>'
--- server/seed.py
+++ server/seed.py
@@ -10,28 +10,30 @@ from faker import Faker
 from app import app
 from models import db, User, Edit, Page
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
-        #clear database
+        # clear database
         User.query.delete()
         Edit.query.delete()
         Page.query.delete()
-        print('Datebase Cleared')
-        marc = User(username="Marc")
-        yu = User(username="Yu")
-        tj = User(username="TJ")
-        
-        page1 = Page(title='title1', text='text1\n')
-        page2 = Page(title='title2', text='text2\n')
-        page3 = Page(title='title3', text='text3\n')
-        
+        print("Datebase Cleared")
+        marc = User(username="marc123", password_hash="123456")
+        yu = User(username="yu1234", password_hash="abcdef")
+        tj = User(username="tj1234", password_hash="qweasd")
+
+        page1 = Page(title="title1", text="text1\n")
+        page2 = Page(title="title2", text="text2\n")
+        page3 = Page(title="title3", text="text3\n")
+
         # edit1 = Edit(user_id=1, page_id=1, diff='diff1')
         # edit2 = Edit(user_id=2, page_id=2, diff='diff2')
         # edit3 = Edit(user_id=3, page_id=3, diff='diff3')
         # edit4 = Edit(user_id=1, page_id=3, diff='diff4')
-        
-        db.session.add_all([marc,yu,tj,page1,page2,page3])#,edit1,edit2,edit3,edit4])
+
+        db.session.add_all(
+            [marc, yu, tj, page1, page2, page3]
+        )  # ,edit1,edit2,edit3,edit4])
         db.session.commit()
-        print('Finished Seeding')
+        print("Finished Seeding")
